65e09d0a13ffb7b78bd2b933ebbbdba0c68bd185
DevQNguyen <go.quang.nguyen@gmail.com>
2021-03-19 17:04:41 -0700

    L43: Add script to rocket for collision detection. Implement using switch statement.

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class CollisionHandler : MonoBehaviour
+{
+    void OnCollisionEnter(Collision other)
+    {
+        // Grab object tag name and assign to variable
+        string tagString = other.gameObject.tag;
+        
+        switch (tagString)
+        {
+            case "Friendly":
+                Debug.Log("Collided with a Friendly object!");
+                break;
+            case "Fuel":
+                Debug.Log("You got a fuel pod!");
+                break;
+            case "Finish":
+                Debug.Log("You have successfully finished the course!");
+                break;
+            default:
+                Debug.Log("Collided with some other object!");
+                break;
+        }
+    }
+}
+
+
+
