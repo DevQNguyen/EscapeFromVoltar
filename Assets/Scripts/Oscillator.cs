dc014b488121fc1479196c59ceb17056345941a2
DevQNguyen <go.quang.nguyen@gmail.com>
2021-03-31 15:01:36 -0700

    L55-56. Place spot and point lighting in scenes. Script the basics of oscillator for obstacles.

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+    [SerializeField][Range(0, 1f)] float movementFactor;
+    [SerializeField] Vector3 movementVector;
+    Vector3 startPos;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Cache starting transform position
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Calculate and cache offset position
+        Vector3 offsetPos = movementFactor * movementVector;
+        Debug.Log($"Offset: {offsetPos}");
+        
+        // Move transform to new position by adding offset value
+        transform.position = startPos + offsetPos;
+        Debug.Log($"End Position: {transform.position}");
+        
+    }
+}
+
+
+
